de128aad48c384c0324f6c81348d1ada17a173eb
nikitakumala-24 <nikitakisaran@gmail.com>
2025-11-15 21:49:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function home(){
+    document.getElementById('selamat').innerHTML="Selamat Datang, Nama Mahasiswa Kelas SI-3X";
+    document.getElementById('aktifi').style.backgroundColor="white";
+    document.getElementById('aktifi1').style.backgroundColor="white";
+    document.getElementById('aside').style.backgroundColor="white";
+    document.getElementById('lmpr').style.backgroundColor="white";
+}
+
+function mahasiswa () {
+    document.getElementById('selamat').innerHTML="Selamat Datang, Nikita Kumala Sari ";
+}
+
+function kelas() {
+    document.getElementById('selamat').innerHTML="Selamat Datang, Nikita Kumala Sari Kelas SI-3C";
+}
+
+function aktifitas(){
+    document.getElementById('aktifi').style.backgroundColor="#1d00fcff";
+    document.getElementById('aktifi1').style.backgroundColor="#4400ffff";
+    document.getElementById('aside').style.backgroundColor="white";
+    document.getElementById('lmpr').style.backgroundColor="white";
+}
+
+function statistik(){
+    document.getElementById('aside').style.backgroundColor="#e35b5bff";
+    document.getElementById('lmpr').style.backgroundColor="white";
+    document.getElementById('aktifi').style.backgroundColor="white";
+    document.getElementById('aktifi1').style.backgroundColor="white";
+    
+}
+
+function dokumen(){
+    document.getElementById('lmpr').style.backgroundColor="#000153ff";
+    backgroundColor="white";
+    document.getElementById('aktifi').style.backgroundColor="white";
+    document.getElementById('aktifi1').style.backgroundColor="white";
+    document.getElementById('aside').style.backgroundColor="white";
+}
+
+function singout(){
+    document.getElementById('selamat').innerHTML="Selamat Datang, Nama Mahasiswa Kelas";
+    document.getElementById('aktifi').style.backgroundColor="white";
+    document.getElementById('aktifi1').style.backgroundColor="white";
+    document.getElementById('aside').style.backgroundColor="white";
+    document.getElementById('lmpr').style.backgroundColor="white";
+}
